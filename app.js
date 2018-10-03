4297ff857126693141125b1a95a1297d7f01e045
Jeremiah Ogutu <jeremiahogutu@ptotonmail.com>
2018-10-03 09:06:55 -0400

    callback functions and Apis, also added request to package.json

---
+++ app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+    url: 'https://www.metaweather.com/api/location/4118/',
+    json: true
+}, (error, response, body) => {
+    console.log(body);
+});
