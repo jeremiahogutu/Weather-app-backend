b4b96346c6343f61bbfa424e6af003457d855804
Jeremiah Ogutu <jeremiahogutu@ptotonmail.com>
2018-10-03 09:11:00 -0400

    pretty printing objects

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://www.metaweather.com/api/location/4118/',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(body, undefined, 2);
 });
