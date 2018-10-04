1b6fb3ee3219a0f056ea3b5f0c3721b7fb7b561d
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-04 09:20:35 -0400

    implemented a geo-location api

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const request = require('request');
 
 request({
-    url: 'https://www.metaweather.com/api/location/2471217/',
+    url: 'https://geocoder.api.here.com/6.2/geocode.json?searchtext=1301%20Lombard%20St,%20Philadelphia,%20PA%2019147,%20USA&app_id=gsYbZt1D0vdlrOheGMhh&app_code=-XfaXYriTAhKLHyxaU8_gg&gen=8',
     json: true
 }, (error, response, body) => {
-    const latitude = body.parent.latt_long.toString().split(',')[0];
-    const longitude = body.parent.latt_long.toString().split(',')[1];
-    console.log(`Latitude: ${latitude}`);
-    console.log(`Longitude: ${longitude}`)
+    console.log(`Address: ${body.Response.View[0].Result[0].Location.Address.Label}`);
+    console.log(`Latitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Latitude}`);
+    console.log(`Longitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`);
 });
