24bda34e82f7c0347ff4eb1114b4bc7e9ae8102a
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-05 22:35:11 -0400

    encoding user component

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+})
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://geocoder.api.here.com/6.2/geocode.json?searchtext=1301%20Lombard%20St,%20Philadelphia,%20PA%2019147,%20USA&app_id=gsYbZt1D0vdlrOheGMhh&app_code=-XfaXYriTAhKLHyxaU8_gg&gen=8',
+    url: `https://geocoder.api.here.com/6.2/geocode.json?searchtext=${encodedAddress}&app_id=gsYbZt1D0vdlrOheGMhh&app_code=-XfaXYriTAhKLHyxaU8_gg&gen=8`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.Response.View[0].Result[0].Location.Address.Label}`);
