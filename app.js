7a17039b28392e37f1b57ebcbcf1432a313c44c7
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-05 22:58:52 -0400

    callback errors

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ request({
     url: `https://geocoder.api.here.com/6.2/geocode.json?searchtext=${encodedAddress}&app_id=gsYbZt1D0vdlrOheGMhh&app_code=-XfaXYriTAhKLHyxaU8_gg&gen=8`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.Response.View[0].Result[0].Location.Address.Label}`);
-    console.log(`Latitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Latitude}`);
-    console.log(`Longitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`);
+    if (error) {
+        console.log('unable to connect to sever')
+    } else if (body.Response === undefined) {
+        console.log('Unable to find address.')
+    } else  {
+        console.log(`Address: ${body.Response.View[0].Result[0].Location.Address.Label}`);
+        console.log(`Latitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Latitude}`);
+        console.log(`Longitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`);
+    }
 });
