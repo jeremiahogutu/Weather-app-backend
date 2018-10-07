ef917491d6918fc3c611374e561b7ecf7bb91678
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-06 23:04:24 -0400

    chaining callbacks together

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
-const request = require('request');
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-// const yargs = require('yargs');
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs
-//     .options({
-//         a: {
-//             demand: true,
-//             alias: 'address',
-//             describe: 'Address to fetch weather for',
-//             string: true
-//         }
-// })
-//     .help()
-//     .alias('help', 'h')
-//     .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         console.log(errorMessage)
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-// });
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+})
+    .help()
+    .alias('help', 'h')
+    .argv;
 
-// ddec24402bd1b9e72a119ae60c3cbd29
-
-request({
-    url: `https://api.darksky.net/forecast/ddec24402bd1b9e72a119ae60c3cbd29/43.47189,-80.51233`,
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(body.currently.temperature)
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage)
     } else {
-        console.log('Unable to fetch weather.');
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage)
+            } else {
+                console.log(`It's currently ${ Math.round((weatherResults.temperature - 32) * (5/9) * 10)/10 }. It feels like ${Math.round((weatherResults.apparentTemperature - 32) * (5/9) * 10)/10}`)
+            }
+        });
     }
 });
+
