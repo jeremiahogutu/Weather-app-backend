c2ba6d0667fb4ad6dfea3e8b4256bd9ea6ae8b16
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-07 09:45:46 -0400

    changed weather app to be promise based using axios

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+const axios = require('axios');
 
 const argv = yargs
     .options({
@@ -10,23 +9,27 @@ const argv = yargs
             describe: 'Address to fetch weather for',
             string: true
         }
-})
+    })
     .help()
     .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage)
-    } else {
-        console.log(results.address);
-        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-            if (errorMessage) {
-                console.log(errorMessage)
-            } else {
-                console.log(`It's currently ${ Math.round((weatherResults.temperature - 32) * (5/9) * 10)/10 }. It feels like ${Math.round((weatherResults.apparentTemperature - 32) * (5/9) * 10)/10}`)
-            }
-        });
-    }
-});
+const encodedAddress = encodeURIComponent(argv.address);
+const geocodeUrl = `https://geocoder.api.here.com/6.2/geocode.json?searchtext=${encodedAddress}&app_id=gsYbZt1D0vdlrOheGMhh&app_code=-XfaXYriTAhKLHyxaU8_gg&gen=8`;
 
+axios.get(geocodeUrl).then((body) => {
+    if (body.data.Response === undefined) {
+        throw new Error('Unable to find that address.')
+    }
+    const longitude = body.data.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
+    const latitude = body.data.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+    const weatherUrl = `https://api.darksky.net/forecast/ddec24402bd1b9e72a119ae60c3cbd29/${latitude},${longitude}`;
+    console.log(body.data.Response.View[0].Result[0].Location.Address.Label);
+    return axios.get(weatherUrl)
+}).then((response) => {
+    const temperature = response.data.currently.temperature;
+    const apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`Its currently ${Math.round((temperature-32*(5/9))*10)/10}. It feels like ${Math.round((apparentTemperature-32*(5/9))*10)/10}.`)
+}).catch((e) => {
+    console.log(e.message)
+});
--- geocode/geocode.js
+++
@@ -1,7 +0,0 @@
-const request = require('request');
-
-const geocodeAddress = (address, callback) => {
-
-};
-
-module.exports.geocodeAddress = geocodeAddress;
