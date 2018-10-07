114f52e99eb605161f4899bad5a4779dd9b963c7
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-06 20:26:23 -0400

    Abstracting callbacks

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,19 +14,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-const encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://geocoder.api.here.com/6.2/geocode.json?searchtext=${encodedAddress}&app_id=gsYbZt1D0vdlrOheGMhh&app_code=-XfaXYriTAhKLHyxaU8_gg&gen=8`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('unable to connect to sever')
-    } else if (body.Response === undefined) {
-        console.log('Unable to find address.')
-    } else  {
-        console.log(`Address: ${body.Response.View[0].Result[0].Location.Address.Label}`);
-        console.log(`Latitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Latitude}`);
-        console.log(`Longitude: ${body.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage)
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
